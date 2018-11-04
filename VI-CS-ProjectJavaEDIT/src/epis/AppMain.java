9d2e0e0e964f25f316509c8b0bb2ae88a39f3b89
DIEGO <marazadiego@gmail.com>
2018-11-04 16:51:11 -0500

    acabando bm

--- VI-CS-ProjectJavaEDIT/src/epis/AppMain.java
+++ VI-CS-ProjectJavaEDIT/src/epis/AppMain.java
@@ -4,6 +4,6 @@ public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		//mi primer comentario
-		//mi primer comentario mmar
+		//mi primer comentario mmarasodaskñmdñaskm
 	}
 }
