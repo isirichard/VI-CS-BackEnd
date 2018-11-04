032da730b8cb3a4ecb10e10216e036ffdbf48299
yonathan <yhona777@gmail.com>
2018-11-04 16:57:37 -0500

    yoaaaa

--- VI-CS-ProjectJavaEDIT/src/epis/AppMain.java
+++ VI-CS-ProjectJavaEDIT/src/epis/AppMain.java
@@ -4,13 +4,15 @@ public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		//mi primer comentario
-<<<<<<< HEAD
+
 
 		//mi segundo comentario
 
 		//mi primer comentario mmaraza
-=======
+
 		//mi primer comentario mmar
->>>>>>> 355410e83ad3320092c92f80dfd7b69d7a350fde
+		
+		//mi ultimo comentario
+
 	}
 }
