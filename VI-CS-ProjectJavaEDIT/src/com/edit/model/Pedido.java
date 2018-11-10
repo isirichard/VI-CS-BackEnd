49db96a0eff760fd707f887c2b3ec3dcc61c660d
DIEGO <marazadiego@gmail.com>
2018-11-10 17:10:59 -0500

    acabando rama Maraza

---
+++ VI-CS-ProjectJavaEDIT/src/com/edit/model/Pedido.java
@@ -0,0 +1,40 @@
+package com.edit.model;
+
+import java.util.List;
+
+public class Pedido {
+
+	private List<Producto> DetProd;
+	private int DetCan;
+	private double DetPreTot;
+	private Inventario DetInv;
+	public List<Producto> getDetProd() {
+		return DetProd;
+	}
+	public void setDetProd(List<Producto> detProd) {
+		DetProd = detProd;
+	}
+	public int getDetCan() {
+		return DetCan;
+	}
+	public void setDetCan(int detCan) {
+		DetCan = detCan;
+	}
+	public double getDetPreTot() {
+		return DetPreTot;
+	}
+	public void setDetPreTot(double detPreTot) {
+		DetPreTot = detPreTot;
+	}
+	public Inventario getDetInv() {
+		return DetInv;
+	}
+	public void setDetInv(Inventario detInv) {
+		DetInv = detInv;
+	}
+	public Pedido() {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
