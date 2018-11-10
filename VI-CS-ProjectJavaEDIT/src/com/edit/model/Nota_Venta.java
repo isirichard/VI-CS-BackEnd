438ee78dc7de609501522034ba0c399898847261
yonathan <yhona777@gmail.com>
2018-11-10 17:50:37 -0500

    yoni1

---
+++ VI-CS-ProjectJavaEDIT/src/com/edit/model/Nota_Venta.java
@@ -0,0 +1,34 @@
+package com.edit.model;
+
+public class Nota_Venta {
+	
+	private Cliente NotVenCli;
+	private Date NotVenFecEnt;
+	
+	public Nota_Venta(){
+		
+	};
+	
+	public Cliente getCliente(){
+		return NotVenCli;
+	}
+	
+	public Date getFechaEntrega(){
+		return NotVenFecEnt;
+	}
+	
+	public void verificarTipoCliente() {	
+	}
+	
+	public void verificarSaldoCliente() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
