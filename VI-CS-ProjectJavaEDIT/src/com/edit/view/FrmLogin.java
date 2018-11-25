4f11da95933c0ecf28349e3e52b0702c2220778e
isirichard <isi.12ichard@gmail.com>
2018-11-24 22:54:59 -0500

    final

--- VI-CS-ProjectJavaEDIT/src/com/edit/view/FrmLogin.java
+++ VI-CS-ProjectJavaEDIT/src/com/edit/view/FrmLogin.java
@@ -82,10 +82,12 @@ public class FrmLogin extends JFrame {
 		
 	}
 	private void btnIngresarAccion(ActionEvent e) {
-		MDIPrincipal principal = new MDIPrincipal();
-		principal.setVisible(true);
-		setLocationRelativeTo(null);
-		this.dispose();
+		if(miCoordinador.validar(txtUsuario.getText())) {
+			MDIPrincipal principal = new MDIPrincipal();
+			principal.setVisible(true);
+			setLocationRelativeTo(null);
+			this.dispose();
+		}
 	}
 	private void btnCancelarAccion(ActionEvent e) {
 		System.exit(0);
